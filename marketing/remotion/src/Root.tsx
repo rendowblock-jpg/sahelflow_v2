f2f176aafa529486b944b4b7795a812ae1e9adfb
rendowblock-jpg <rendowblock@gmail.com>
2026-07-18 00:29:44 +0100

    feat(video): register SahelFlow video compositions

---
+++ marketing/remotion/src/Root.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Composition, Folder} from 'remotion';
+import {DemoFilm90, LaunchFilm60, SocialCut30, VerticalCut15} from './compositions';
+
+const FPS = 30;
+
+export const RemotionRoot: React.FC = () => (
+  <>
+    <Folder name="SahelFlow Launch">
+      <Composition
+        id="SahelFlow-Launch-60"
+        component={LaunchFilm60}
+        durationInFrames={60 * FPS}
+        fps={FPS}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="SahelFlow-Social-30"
+        component={SocialCut30}
+        durationInFrames={30 * FPS}
+        fps={FPS}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="SahelFlow-Vertical-15"
+        component={VerticalCut15}
+        durationInFrames={15 * FPS}
+        fps={FPS}
+        width={1080}
+        height={1920}
+      />
+    </Folder>
+    <Folder name="SahelFlow Product Demo">
+      <Composition
+        id="SahelFlow-Demo-90"
+        component={DemoFilm90}
+        durationInFrames={90 * FPS}
+        fps={FPS}
+        width={1920}
+        height={1080}
+      />
+    </Folder>
+  </>
+);
