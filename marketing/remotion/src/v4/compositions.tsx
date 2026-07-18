8dd248d8009ac0b7c4cb75f24a88e8a32af8568a
rendowblock-jpg <rendowblock@gmail.com>
2026-07-18 09:13:30 +0100

    fix(video): route V4 masters through refined edit

--- marketing/remotion/src/v4/compositions.tsx
+++ marketing/remotion/src/v4/compositions.tsx
@@ -1,1 +1,12 @@
-export {V4Hero48,V4Social20,V4Vertical12,V4Demo75,V4LandingLoop8,V4_HERO,V4_SOCIAL,V4_VERTICAL,V4_DEMO,V4_LOOP} from './film';
+export {
+  V4Hero48,
+  V4Social20,
+  V4Vertical12,
+  V4Demo75,
+  V4LandingLoop8,
+  V4_HERO,
+  V4_SOCIAL,
+  V4_VERTICAL,
+  V4_DEMO,
+  V4_LOOP,
+} from './refined';
