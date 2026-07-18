310179db039b385557b184a222d71d5feab9f7cf
rendowblock-jpg <rendowblock@gmail.com>
2026-07-18 03:15:37 +0100

    feat(video): register premium V2 compositions

--- marketing/remotion/src/Root.tsx
+++ marketing/remotion/src/Root.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
 import {Composition, Folder} from 'remotion';
 import {DemoFilm90, LaunchFilm60, SocialCut30, VerticalCut15} from './compositions';
+import {
+  PREMIUM_DEMO_DURATION,
+  PREMIUM_LAUNCH_DURATION,
+  PREMIUM_SOCIAL_DURATION,
+  PREMIUM_VERTICAL_DURATION,
+  PremiumDemo90,
+  PremiumLaunch60,
+  PremiumSocial30,
+  PremiumVertical15,
+} from './v2/compositions';
 
 const FPS = 30;
 
 export const RemotionRoot: React.FC = () => (
   <>
-    <Folder name="SahelFlow-Launch">
+    <Folder name="SahelFlow-Premium-V2">
+      <Composition
+        id="SahelFlow-Premium-Launch-60"
+        component={PremiumLaunch60}
+        durationInFrames={PREMIUM_LAUNCH_DURATION}
+        fps={FPS}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="SahelFlow-Premium-Social-30"
+        component={PremiumSocial30}
+        durationInFrames={PREMIUM_SOCIAL_DURATION}
+        fps={FPS}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="SahelFlow-Premium-Vertical-15"
+        component={PremiumVertical15}
+        durationInFrames={PREMIUM_VERTICAL_DURATION}
+        fps={FPS}
+        width={1080}
+        height={1920}
+      />
+      <Composition
+        id="SahelFlow-Premium-Demo-90"
+        component={PremiumDemo90}
+        durationInFrames={PREMIUM_DEMO_DURATION}
+        fps={FPS}
+        width={1920}
+        height={1080}
+      />
+    </Folder>
+    <Folder name="SahelFlow-Legacy-V1">
       <Composition
         id="SahelFlow-Launch-60"
         component={LaunchFilm60}
@@ -31,8 +75,6 @@ export const RemotionRoot: React.FC = () => (
         width={1080}
         height={1920}
       />
-    </Folder>
-    <Folder name="SahelFlow-Product-Demo">
       <Composition
         id="SahelFlow-Demo-90"
         component={DemoFilm90}
